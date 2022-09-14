66c3381ba1e7ab692bc7f66f39fc472da4192a64
kaiete <kaiete@aragon-press.com>
2022-09-14 17:03:36 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,43 @@
+function whichmon () {
+    if (area == 0) {
+        currentmon = normalmons._pickRandom()
+    } else if (area == 1) {
+        currentmon = mosscavemons._pickRandom()
+    } else {
+        currentmon = oceanmons._pickRandom()
+    }
+    basic.showString("" + (currentmon))
+    if (dex.indexOf(currentmon) == -1) {
+        dex.push(currentmon)
+    }
+    if (dex.length == 9) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            `)
+        basic.showString("A... Pokemon?")
+        basic.showString("Rayquaza?")
+        basic.showString("Shake to throw a Poke Ball!")
+        rayquazaencounter = true
+    }
+}
+input.onPinPressed(TouchPin.P0, function () {
+    character.delete()
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showString("Moss Cave")
+    area = 1
+    character = game.createSprite(2, 2)
+})
 function encounterwild () {
     character.delete()
     led.setBrightness(5)
@@ -19,13 +59,7 @@ function encounterwild () {
             `)
     }
     encountermon = randint(1, 3)
-    if (1 == encountermon) {
-        basic.showString("Diglett")
-    } else if (2 == encountermon) {
-        basic.showString("Pikachu")
-    } else {
-        basic.showString("Ratatta")
-    }
+    whichmon()
     _catch = randint(1, 8)
     if (5 <= _catch) {
         basic.showLeds(`
@@ -102,20 +136,96 @@ input.onButtonPressed(Button.A, function () {
         encounterwild()
     }
 })
+input.onPinPressed(TouchPin.P2, function () {
+    character.delete()
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showString("Route 1")
+    area = 0
+    character = game.createSprite(2, 2)
+})
 input.onButtonPressed(Button.AB, function () {
     character.delete()
+    basic.showString("Caught:")
     basic.showNumber(game.score())
+    basic.showString("Pokedex: " + dex.length)
+    if (dex.length == 10) {
+        basic.showIcon(IconNames.Heart)
+        basic.showString("Full Dex!")
+    }
     game.setScore(0)
     control.reset()
 })
 input.onButtonPressed(Button.B, function () {
     character.turn(Direction.Right, 90)
 })
+input.onPinPressed(TouchPin.P1, function () {
+    character.delete()
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . . . . .
+        . . # . .
+        `)
+    basic.pause(1000)
+    basic.showString("Ocean")
+    area = 2
+    character = game.createSprite(2, 2)
+})
+input.onGesture(Gesture.Shake, function () {
+    if (rayquazaencounter) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        if (1 == randint(1, 10)) {
+            rayquazaencounter = false
+            basic.showIcon(IconNames.Surprised)
+            basic.showString("You caught Rayquaza!")
+            dex.push("Rayquaza")
+            character.delete()
+            basic.showString("Caught:")
+            basic.showNumber(game.score())
+            basic.showString("Pokedex: " + dex.length)
+            if (dex.length == 10) {
+                basic.showIcon(IconNames.Heart)
+                basic.showString("Full Dex!")
+            }
+            game.setScore(0)
+            control.reset()
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    }
+})
 let _catch = 0
 let encountermon = 0
+let currentmon = ""
 let character: game.LedSprite = null
+let area = 0
+let oceanmons: string[] = []
+let rayquazaencounter = false
+let mosscavemons: string[] = []
+let dex: string[] = []
+let normalmons: string[] = []
+normalmons = ["Diglett", "Pikachu", "Ratatta"]
+dex = []
+mosscavemons = ["Bellsprout", "Blipbug", "Caterpie"]
+rayquazaencounter = false
+oceanmons = ["Magikarp", "Finneon", "Wishiwashi"]
+area = 0
 basic.showString("GameBit")
-for (let index = 0; index < 3; index++) {
+for (let index = 0; index < 1; index++) {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -141,4 +251,11 @@ for (let index = 0; index < 3; index++) {
 character = game.createSprite(2, 2)
 basic.forever(function () {
     character.ifOnEdgeBounce()
+})
+basic.forever(function () {
+    while (rayquazaencounter) {
+        if (!(character.isDeleted())) {
+            character.delete()
+        }
+    }
 })
